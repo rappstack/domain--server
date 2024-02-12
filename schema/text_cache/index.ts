c83c8ae581a00842b41db6c09b00c4bce42d9361
Brian Takita <brian.takita@gmail.com>
2024-02-12 07:21:51 -0500

    drizzle:
    	- drizzle_schema:
    		- drizzle_schema$_
    		- drizzle_schema_
    		- drizzle_schema__set
    	drizzle_db: drizzle(sqlite_db)
    
    text_cache:
    
    	timestamp_ms→create_ms:
    		+ notNull
    		+ default: CURRENT_TIMESTAMP
    	+ validate_ms
    	data: + notNull

--- schema/text_cache/index.ts
+++ schema/text_cache/index.ts
@@ -1,6 +1,14 @@
+import { sql } from 'drizzle-orm'
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 export const text_cache = sqliteTable('text_cache', {
-	cache_id: text('cache_id').notNull().primaryKey(),
-	timestamp_ms: integer('timestamp_ms', { mode: 'timestamp_ms' }),
-	data: text('data'),
+	text_cache_id: text('text_cache_id').notNull().primaryKey(),
+	create_ms:
+		integer('create_ms', { mode: 'timestamp_ms' })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`),
+	validate_ms:
+		integer('validate_ms', { mode: 'timestamp_ms' })
+			.notNull()
+			.default(sql`CURRENT_TIMESTAMP`),
+	data: text('data').notNull(),
 })
