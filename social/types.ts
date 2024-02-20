dc8649143fc37ff4d5d05b584b8274d90566b069
Brian Takita <brian.takita@gmail.com>
2024-02-20 15:10:18 -0500

    ./social:
    	social_T:
    		- name:social_media_T
    		+ icon_html: { toString():string }
    	- social_icons_T
    	- social_media_T

--- social/types.ts
+++ social/types.ts
@@ -1,30 +1,6 @@
 export type social_T = {
-	name:social_media_T
+	icon_html:{ toString():string }
 	href:string
 	active:boolean
 	link_title:string
 }
-export type social_icons_T = {
-	[social in social_media_T]:string
-}
-export type social_media_T =
-	|'Github'
-	|'Facebook'
-	|'Instagram'
-	|'LinkedIn'
-	|'Mail'
-	|'Twitter'
-	|'Twitch'
-	|'YouTube'
-	|'WhatsApp'
-	|'Snapchat'
-	|'Pinterest'
-	|'TikTok'
-	|'CodePen'
-	|'Discord'
-	|'GitLab'
-	|'Reddit'
-	|'Skype'
-	|'Steam'
-	|'Telegram'
-	|'Mastodon'
