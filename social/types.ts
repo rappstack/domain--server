21655068f5ecb32bbf8de81662829d8a9edc7657
Brian Takita <brian.takita@gmail.com>
2024-02-20 19:19:08 -0500

    ./social: social_T: icon_html→icon_:($p?: { class?:string })=>tag_dom_T
    ∋ relementjs

--- social/types.ts
+++ social/types.ts
@@ -1,5 +1,6 @@
+import { type tag_dom_T } from 'relementjs'
 export type social_T = {
-	icon_html:{ toString():string }
+	icon_:($p?:{ class?:string })=>tag_dom_T
 	href:string
 	active:boolean
 	link_title:string
