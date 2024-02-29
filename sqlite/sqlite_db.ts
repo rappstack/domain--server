6c2d6537fd7746e72670d3d0e1b676b05f5cb844
Brian Takita <brian.takita@gmail.com>
2024-02-28 23:18:06 -0500

    ./sqlite:
    	- sqlite_db__name_
    	- sqlite_db__name__set
    	+ sqlite_db__set
    
    update dependencies

--- sqlite/sqlite_db.ts
+++ sqlite/sqlite_db.ts
@@ -1,23 +1,10 @@
 import { Database } from 'bun:sqlite'
-import { ns_id_be_memo_pair_, ns_id_be_sig_triple_, nullish__none_ } from 'ctx-core/rmemo'
-export const [
-	,
-	sqlite_db__name_,
-	sqlite_db__name__set,
-] = ns_id_be_sig_triple_<string|undefined, 'app'>(
-	'app',
-	'sqlite_db__name',
-	()=>undefined)
+import { ns_id_be_sig_triple_ } from 'ctx-core/rmemo'
 export const [
 	,
 	sqlite_db_,
-] = ns_id_be_memo_pair_(
+	sqlite_db__set,
+] = ns_id_be_sig_triple_(
 	'app',
 	'sqlite_db',
-	ctx=>
-		nullish__none_([sqlite_db__name_(ctx)],
-			sqlite_db__name=>{
-				const sqlite_db = new Database(sqlite_db__name)
-				sqlite_db.exec('PRAGMA journal_mode = WAL;')
-				return sqlite_db
-			}))
+	()=><Database|undefined>undefined)
