11797ef0bf75e11ff7c3af2d24e40eeeb0a7fe60
Brian Takita <brian.takita@gmail.com>
2024-03-04 10:59:19 -0500

    ./ctx: site_request_ctx__ensure_config_T: + logo_image_?:logo_image__T: - logo_image
    ./logo:
    
    	+ logo_image__new_
    	+ logo_image__new__set
    	+ logo_image__new_T
    	- logo_image_
    	- logo_image__set
    	- logo_image_T
    	- logo_image__enable_
    	- logo_image__height_
    	- logo_image__src_
    	- logo_image__width_
    
    ∋ rebuildjs

--- logo/logo_image.ts
+++ logo/logo_image.ts
@@ -1,15 +1,13 @@
-import { id_be_sig_triple_ } from 'relementjs'
+import { id_be_sig_triple_, type tag_dom_T } from 'relementjs'
+import { type wide_ctx_T } from 'rmemo'
 export const [
 	,
-	logo_image_,
-	logo_image__set,
-] = id_be_sig_triple_<logo_image_T|undefined>(
+	logo_image__new_,
+	logo_image__new__set,
+] = id_be_sig_triple_<logo_image__new_T|undefined>(
 	'logo_image',
 	()=>undefined)
-export interface logo_image_T {
-	enable:boolean
-	svg:boolean
-	src?:string
-	width:number
-	height:number
+export function logo_image_(ctx:wide_ctx_T, $p?:{ class?:string }) {
+	return logo_image__new_(ctx)?.($p)
 }
+export type logo_image__new_T = ($p?:{ class?:string })=>tag_dom_T
