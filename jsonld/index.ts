2ad0ed340889520b793ab94c5ac36db168ea3e40
Brian Takita <brian.takita@gmail.com>
2024-03-07 20:33:18 -0500

    ./ld_json→./jsonld:
    	+ jsonld_Graph_
    	+ app_jsonld__add
    	+ jsonld__add
    	- site__ld_json_
    	- site__ld_json__set
    
    ∋ @btakita/schema-dts
    
    update dependencies

---
+++ jsonld/index.ts
@@ -0,0 +1,24 @@
+import type { Graph, Thing } from '@btakita/schema-dts'
+import { id_be_, ns_id_be_ } from 'ctx-core/rmemo'
+import { type request_ctx_T, wide_app_ctx_T } from 'rebuildjs/server'
+export const jsonld_Graph_ = ns_id_be_('app', 'site__jsonld_Graph', (ctx:request_ctx_T)=>{
+	return <Graph>{
+		'@context': 'https://schema.org',
+		'@graph': [...app_jsonld_Thing_S_(ctx), ...jsonld_Thing_S_(ctx)]
+	}
+})
+const app_jsonld_Thing_S_ = ns_id_be_('app', 'app_jsonld_thing_S', ctx=>
+	new Set<Thing>())
+export function app_jsonld__add(ctx:wide_app_ctx_T, thing:Thing):{ '@id':string } {
+	app_jsonld_Thing_S_(ctx).add(thing)
+	return id_ref_(thing)
+}
+const jsonld_Thing_S_ = id_be_('jsonld_thing_S', ctx=>
+	new Set<Thing>())
+export function jsonld__add(ctx:request_ctx_T, thing:Thing):{ '@id':string } {
+	jsonld_Thing_S_(ctx).add(thing)
+	return id_ref_(thing)
+}
+function id_ref_(thing:Thing):{ '@id':string } {
+	return { '@id': <string>(<never>thing)['@id'] }
+}
