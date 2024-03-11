d33c5b734d27b831ed53ebc7328960cf9ee6febf
Brian Takita <brian.takita@gmail.com>
2024-03-11 03:01:04 -0400

    ./jsonld:
    	ns_id_id_ref_be_jsonld_pair_→ns_id_be_id_ref_be_jsonld_pair_:
    	id_be_id_ref_jsonld_pair_:
    		fix: duplicate jsonld entities
    	automatically load a WebPage_id_ref_:
    		WebPage__name_
    		WebPage__name__set
    		WebPage__description_
    		WebPage__description__set
    		WebPage__type_
    		WebPage__type__set

--- jsonld/index.ts
+++ jsonld/index.ts
@@ -7,7 +7,7 @@ import { id_be_sig_triple_ } from 'relementjs'
 import { type wide_ctx_T } from 'rmemo'
 import { request_url__pathname_ } from '../request/index.js'
 import { site__website_ } from '../site/index.js'
-export function ns_id_id_ref_be_jsonld_pair_<
+export function ns_id_be_id_ref_be_jsonld_pair_<
 	T extends Thing,
 	ns_T extends ''|'app',
 	ctx_T extends (wide_app_ctx_T|request_ctx_T)&wide_ctx_T<ns_T>
@@ -19,15 +19,15 @@ export function ns_id_id_ref_be_jsonld_pair_<
 	const [, jsonld_id_ref_] = ns_id_be_memo_pair_(ns, id + '_id_ref', (ctx:ctx_T)=>{
 		return (
 			ns === 'app'
-				? app_jsonld__add(ctx, fn)
-				: jsonld__add(ctx, fn)
+				? app_jsonld__add<ctx_T, T>(ctx, fn)
+				: jsonld__add(<request_ctx_T>ctx, <(ctx:request_ctx_T)=>T>fn)
 		)
 	})
 	const [, jsonld_] = ns_id_be_memo_pair_(ns, id, (ctx:ctx_T)=>{
 		return <T>(
 			ns === 'app'
-				? app_id_ref_M_jsonld_(ctx).get(jsonld_id_ref_(ctx))
-				: id_ref_M_jsonld_(<wide_ctx_T>ctx).get(jsonld_id_ref_(ctx))
+				? app_id_ref_M_jsonld_(ctx).get(<(ctx:wide_app_ctx_T)=>T>fn)
+				: id_ref_M_jsonld_(<wide_ctx_T>ctx).get(<(ctx:request_ctx_T)=>T>fn)
 		)
 	})
 	return [
@@ -39,7 +39,7 @@ export function id_be_id_ref_jsonld_pair_<
 	val_T extends Thing,
 	ctx_T extends request_ctx_T
 >(id:string, fn:(ctx:ctx_T)=>val_T) {
-	return ns_id_id_ref_be_jsonld_pair_<val_T, '', ctx_T>('', id, fn)
+	return ns_id_be_id_ref_be_jsonld_pair_<val_T, '', ctx_T>('', id, fn)
 }
 export const jsonld_Graph__init_ = id_be_('jsonld_Graph__init', (ctx:request_ctx_T)=>()=>{
 })
@@ -48,30 +48,30 @@ export const jsonld_Graph_ = id_be_('site__jsonld_Graph', (ctx:request_ctx_T)=>{
 	return <Graph>{
 		'@context': 'https://schema.org',
 		'@graph': [
-			...[...app_id_ref_M_jsonld_(ctx).values()].map(fn=>fn(ctx)),
-			...[...id_ref_M_jsonld_(ctx).values()].map(fn=>fn(ctx))
+			...app_id_ref_M_jsonld_(ctx).values(),
+			...id_ref_M_jsonld_(ctx).values()
 		]
 	}
 })
 const app_id_ref_M_jsonld_ = ns_id_be_('app', 'id_ref_M_jsonld_Thing', ()=>
-	new Map<id_ref_T, (ctx:wide_app_ctx_T)=>Thing>())
+	new Map<(ctx:wide_app_ctx_T)=>Thing, Thing>)
 export function app_jsonld__add<ctx_T extends wide_app_ctx_T, T extends Thing>(
 	ctx:ctx_T,
 	fn:(ctx:ctx_T)=>T
 ):{ '@id':string } {
-	const id_ref = jsonld_id_ref_(fn(ctx))
-	app_id_ref_M_jsonld_(ctx).set(id_ref, fn)
-	return id_ref
+	const thing = fn(ctx)
+	app_id_ref_M_jsonld_(ctx).set(<never>fn, thing)
+	return jsonld_id_ref_(thing)
 }
 const id_ref_M_jsonld_ = id_be_('id_ref_M_jsonld_Thing', ()=>
-	new Map<id_ref_T, (ctx:request_ctx_T)=>Thing>())
+	new Map<(ctx:request_ctx_T)=>Thing, Thing>())
 export function jsonld__add<ctx_T extends request_ctx_T, T extends Thing>(
 	ctx:ctx_T,
 	fn:(ctx:ctx_T)=>T
 ):{ '@id':string } {
-	const id_ref = jsonld_id_ref_(fn(ctx))
-	id_ref_M_jsonld_(ctx).set(id_ref, fn)
-	return id_ref
+	const thing = fn(ctx)
+	id_ref_M_jsonld_(ctx).set(<never>fn, thing)
+	return jsonld_id_ref_(fn(ctx))
 }
 export function jsonld_id_ref_(thing:Thing):id_ref_T {
 	return { '@id': <string>(<never>thing)['@id'] }
@@ -80,17 +80,26 @@ export const [
 	,
 	WebPage__name_,
 	WebPage__name__set
-] = id_be_sig_triple_<string|undefined>('WebPage__name', ()=>undefined)
+] = id_be_sig_triple_<string|undefined, unknown, request_ctx_T>(
+	'WebPage__name',
+	()=>undefined
+).add(ctx=>queueMicrotask(()=>WebPage_id_ref_(ctx)))
 export const [
 	,
 	WebPage__description_,
 	WebPage__description__set
-] = id_be_sig_triple_<string|undefined>('WebPage__description', ()=>undefined)
+] = id_be_sig_triple_<string|undefined, unknown, request_ctx_T>(
+	'WebPage__description',
+	()=>undefined
+).add(ctx=>WebPage__name_(ctx))
 export const [
 	,
 	WebPage__type_,
 	WebPage__type__set
-] = id_be_sig_triple_<WebPage['@type']>('WebPage__type', ()=>'WebPage')
+] = id_be_sig_triple_<WebPage['@type'], unknown, request_ctx_T>(
+	'WebPage__type',
+	()=>'WebPage'
+).add(ctx=>WebPage__name_(ctx))
 export const [
 	[WebPage_id_ref_],
 	WebPage_id_
