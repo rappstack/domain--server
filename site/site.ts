6333579ce3e3db4423e56fe7a3c3697e2c96ae8b
Brian Takita <brian.takita@gmail.com>
2024-03-12 16:41:54 -0400

    ./site:
    	- site__og_image_
    	+ font__meta_props_a1_
    	+ site__icon_
    	+ site__social_image_url_
    	+ site__google_site_verification_
    	+ site__gtag_id_
    	+ site__body_class_
    	site_T:
    		- og_image
    		+ font__meta_props_a1?:font__meta_props_T[]
    		+ icon?:icon_link_props_T
    		+ social_image_url?:string|URL
    		+ google_site_verification?:string
    		+ gtag_id?:string
    		+ body_class?:string
    	+ font__meta_props_T
    	+ icon_link_props_T

--- site/site.ts
+++ site/site.ts
@@ -1,5 +1,7 @@
 import { nullish__none_ } from 'ctx-core/function'
-import { id_be_memo_pair_, id_be_sig_triple_, wide_ctx_T } from 'rmemo'
+import { type request_ctx_T } from 'rebuildjs/server'
+import { request_url_ } from 'relysjs/server'
+import { id_be_memo_pair_, id_be_sig_triple_ } from 'rmemo'
 export const [
 	,
 	site_,
@@ -41,21 +43,86 @@ export const [
 	ctx=>site_(ctx)?.description)
 export const [
 	,
-	site__og_image_,
+	font__meta_props_a1_,
 ] = id_be_memo_pair_(
-	'site__og_image',
-	ctx=>site_(ctx)?.og_image)
+	'font__meta_props_a1',
+	(ctx:request_ctx_T)=>site_(ctx)?.font__meta_props_a1 ?? [])
+export const [
+	,
+	site__icon_,
+] = id_be_memo_pair_(
+	'site__icon',
+	(ctx:request_ctx_T)=>{
+		return nullish__none_([site_(ctx)?.icon], icon=>
+			<icon_link_props_T>{
+				...icon,
+				href: new URL(icon.href, request_url_(ctx).origin) + ''
+			})
+	})
+export const [
+	,
+	site__social_image_url_,
+] = id_be_memo_pair_(
+	'site__social_image_url',
+	ctx=>{
+		return nullish__none_([site_(ctx)?.social_image_url], (social_image_url)=>
+			new URL(social_image_url) + '')
+	})
 export const [
 	,
 	site__light_and_dark_mode_,
 ] = id_be_memo_pair_(
 	'site__light_and_dark_mode',
 	ctx=>site_(ctx)?.light_and_dark_mode)
+export const [
+	,
+	site__google_site_verification_
+] = id_be_memo_pair_(
+	'site__google_site_verification',
+	ctx=>site_(ctx)?.google_site_verification)
+export const [
+	,
+	site__gtag_id_
+] = id_be_memo_pair_(
+	'site__gtag_id',
+	ctx=>site_(ctx)?.gtag_id)
+export const [
+	,
+	site__body_class_
+] = id_be_memo_pair_(
+	'site__body_class',
+	ctx=>site_(ctx)?.body_class)
 export type site_T = {
 	website:string
 	author:string
 	description:string
 	title:string
-	og_image?:string
+	font__meta_props_a1?:font__meta_props_T[]
+	icon?:icon_link_props_T
+	social_image_url?:string|URL
 	light_and_dark_mode?:boolean
+	google_site_verification?:string
+	gtag_id?:string
+	body_class?:string
+}
+export type font__meta_props_T =
+	&{ rel:'preconnect'|'stylesheet', href:string, crossorigin?:1|0 }
+	&(
+	|{ rel:'stylesheet', href:string }
+	|{ rel:'preconnect', href:string, crossorigin?:1|0 }
+	)
+export type icon_link_props_T = {
+	type:
+		|'image/apng'
+		|'image/avif'
+		|'image/bmp'
+		|'image/gif'
+		|'image/jpeg'
+		|'image/png'
+		|'image/svg+xml'
+		|'image/tiff'
+		|'image/webp'
+		|'image/x-icon'
+		|string
+	href:string
 }
